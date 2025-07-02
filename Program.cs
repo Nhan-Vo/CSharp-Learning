7bb2a3cc73fb3c566bf02b0c3e91d14f9cb0a73a
Nhan-Vo <vominhnhannick@gmail.com>
2025-07-02 21:00:49 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        // Create a new instance of the class
+        var myClass = new MyClass();
+
+        // Call the method to get the greeting
+        string greeting = myClass.GetGreeting();
+
+        // Print the greeting to the console
+        Console.WriteLine(greeting);
+    }
+}
+
