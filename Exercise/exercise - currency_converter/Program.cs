02bb772951ffe2d67541f1856a7d6c5751ee2b58
Nhan-Vo <vominhnhannick@gmail.com>
2025-07-07 20:12:29 +0700

    adding more exercises

---
+++ Exercise/exercise - currency_converter/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Converter
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please choose an option:\n1. Convert VND to USD\n2. Convert USD to VND");
+        Console.Write("Enter an option: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        if (number == 1)
+        {
+            Console.Write("-------------------------\nEnter the amount in VND: ");
+            double vnd = Convert.ToDouble(Console.ReadLine());
+            double usd = vnd / 23000;
+            Console.WriteLine($"{vnd} VND = {usd} USD");
+        }
+        else if (number == 2)
+        {
+            Console.Write("-------------------------\nEnter the amount in USD: ");
+            double usd = Convert.ToDouble(Console.ReadLine());
+            double vnd = usd * 23000;
+            Console.WriteLine($"{usd} USD = {vnd} VND");
+        }
+        else
+        {
+            Console.WriteLine("-------------------------\nInvalid option!");
+        }
+    }
+}
---
+++ Exercise/exercise - name_callout/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Name_callout
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        if (string.IsNullOrEmpty(name))
+        {
+            Console.WriteLine("Hello, Mr/Ms. Anonymous");
+            return;
+        }
+        Console.WriteLine("Hello, " + name);
+    }
+}
